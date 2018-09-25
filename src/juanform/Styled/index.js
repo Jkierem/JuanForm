74260a127db02a7b8c4cefb601eeb15c50470e34
jkierem <juanmiguelgomez8@gmail.com>
2018-09-25 09:56:14 -0500

    Added Styled module to more easily apply custom styles

---
+++ src/juanform/Styled/index.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+const Styled = {
+  Button: styled.button,
+  CheckBox: styled.input,
+  ComboBox: styled.select,
+  Field: styled.article,
+  Form: styled.form,
+  Input: styled.input,
+  Label: styled.label,
+  Option: styled.option,
+}
+
+export default Styled;
